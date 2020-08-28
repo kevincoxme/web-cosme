4771054f53f06521a77370cadca27b957f0306be
Kevin Monteza <kevin@cirrena.com>
2020-08-28 11:35:54 +0800

    Convert to static pages

--- wp-content/themes/online-portfolio/searchform.php
+++
@@ -1,6 +0,0 @@
-<?php
-?>
-<form action="<?php echo esc_url(home_url()) ?>" autocomplete="on" class="top-search">
-    <input id="search" name="s" value="<?php echo esc_attr(get_search_query()); ?>" type="text" placeholder="<?php  esc_html_e('Search','online-portfolio')?>&hellip;&hellip;">
-    <div class="search-button"><button type="submit"><?php  esc_html_e('Search','online-portfolio')?></button></div>
-</form>
