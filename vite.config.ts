a0ad3b3b60aa601c4304d36303284cfaaa6f6baf
kevincoxme <kevincoxme26@gmail.com>
2026-05-03 17:43:01 +0800

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'node:path';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/kevincoxme/web-cosme',
+  base: '/web-cosme/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
